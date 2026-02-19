d0deecca83ebf32000889f70bd35005d26206c70
Владимир <petrukhnovk@gmail.com>
2026-02-19 20:04:48 +0600

    Add files via upload

--- yatube_api/posts/admin.py
+++ yatube_api/posts/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Post, Group, Comment, Follow
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('text', 'pub_date', 'author')
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'description')
+    search_fields = ('title',)
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ('user', 'following')
+    search_fields = ('user__username', 'following__username')
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('text', 'author', 'post', 'created')
+    search_fields = ('text',)
+    list_filter = ('created',)
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Group, GroupAdmin)
+admin.site.register(Follow, FollowAdmin)
+admin.site.register(Comment, CommentAdmin)
